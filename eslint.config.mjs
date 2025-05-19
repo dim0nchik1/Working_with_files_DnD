b5da6f52f947a8b789312fcda361e0dca48f73b1
Dim0nchik <dzhikon@bk.ru>
2025-05-19 23:02:22 +0300

    homework

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import jest from "eslint-plugin-jest"
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {ignores:["src/e2e/",".eslintrc.js","webpack.prod.js","webpack.common.js","webpack.dev.js","dist"
+    ]},
+  {files: ['**/*.test.js'],
+    ...jest.configs['flat/recommended'],
+    rules: {
+      ...jest.configs['flat/recommended'].rules,
+
+    }
+  }
+];
